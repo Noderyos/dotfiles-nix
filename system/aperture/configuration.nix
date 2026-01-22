03d9986440691d3de4d20b6b9b0118ba034d19a2
Noderyos <vincent.bsod@gmail.com>
2026-01-22 22:05:57 +0100

    Red theme scheme

--- system/aperture/configuration.nix
+++ system/aperture/configuration.nix
@@ -9,8 +9,22 @@
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "noderyos" ];
 
-  powerManagement.cpuFreqGovernor = "conservative";
   services.usbmuxd.enable = true;
+
+  powerManagement.powertop.enable = true;
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT0 = 99;
+      };
+    };
+  };
 
   hardware.pulseaudio.extraConfig = "
     load-module module-bluetooth-policy auto_switch=false
